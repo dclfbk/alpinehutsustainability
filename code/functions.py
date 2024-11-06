54f2a7da4ff8292dc52011e2f7a0f24204417365
sraatgn <sarategoni18@gmail.com>
2024-11-06 11:20:38 +0100

    created functions.py for general funcs

---
+++ code/functions.py
@@ -0,0 +1,36 @@
+## Script containing functions to import 
+import os
+import requests
+import zipfile
+
+def download_and_unzip_shapefile(url, extract_to='../data/', download=True):
+
+    if not os.path.exists(extract_to):
+        os.makedirs(extract_to)
+
+    if download:
+        # Download the file
+        local_zip_file = os.path.join(extract_to, 'shapefile.zip')
+        with requests.get(url, stream=True) as r:
+            with open(local_zip_file, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    
+    elif not download:
+        local_zip_file = url
+        
+    # Extract the file
+    with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    os.remove(local_zip_file)
+    
+    # Return the path to the directory containing the shapefiles
+    return print(f'Shapefile ready in {extract_to}')
+
+
+def standardize_name(name):
+    ''' Converts names to lowercase, strips leading and trailing whitespace, 
+    and removes non-alphanumeric characters.
+    '''
+    return ''.join(e for e in name.lower().strip() if e.isalnum())
